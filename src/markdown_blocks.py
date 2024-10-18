98e65548f46c7be2bdaa8df81934b3f2ec259ab4
arturogood17 <arturo_good17@hotmail.com>
2024-10-18 23:50:33 +0200

    Block_type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,31 +1,42 @@
+import re
 def markdown_to_blocks(markdown):
     if not markdown:
         return []
-    split = (markdown.split("\n"))
+    split = markdown.split("\n\n")
     new_blocks = []
     for block in split:
         if block:
-          new_blocks.append(block.strip(" "))
-        else:
-            continue
+            new_blocks.append(block.strip())
     return new_blocks
 
-text= """
-
-# Título
-
-   Este es un párrafo con espacios
-al inicio y al final.   
-
-* Primer item
-* Segundo item
-
-
-Otro párrafo después de una línea en blanco.
-
-"""
-
-blocks = markdown_to_blocks(text)
-
-
-print(blocks)
+def block_to_block_type(block):
+    patron_heading = r"^#{1,6} "
+    lines = block.split("\n")
+    if re.match(patron_heading, block):
+        return "heading"
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"): #Tiene que ser len() > 1 porque son cierre y final
+        return "code"
+    if block.startswith("> "):
+        for i in lines:
+            if not i.startswith("> "):
+                return "paragraph"
+        return "quote"
+    
+    if block.startswith("* "):
+        for i in lines:
+            if not i.startswith("* ") :
+                return "paragraph"
+        return "unordered_list"
+    
+    if block.startswith("- "):
+        for i in lines:
+            if not i.startswith("- "):
+                return "paragraph"
+        return "unordered_list"
+    
+    if block.startswith("1. "):
+        for index, value in enumerate(lines[1:]):
+            if not value.startswith(f"{index + 2}. "):
+                return "paragraph"
+        return "ordered_list"
+    return "paragraph"
