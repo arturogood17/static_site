a546f5ef6f7ae5758370b2ca661b3a4ae88ad9bb
arturogood17 <arturo_good17@hotmail.com>
2024-10-22 23:51:56 +0200

    Ajuste a copystatic

--- src/copystatic.py
+++ src/copystatic.py
@@ -9,7 +9,7 @@ def copypath(origen, destino):
         from_path= os.path.join(origen, filename) #guardas origen para ver qué pasa
         dest_path = os.path.join(destino, filename) #guardas destino para ver qué pasa
         print(f"* {from_path} ---> {dest_path}")
-    if os.path.isfile(from_path): #revisas que es un archivo
-        shutil.copy(from_path, dest_path) #si lo es, lo copias
-    else:
-        copypath(from_path, dest_path) #si no, lo recorres y haces el proceso, pero empezando desde los últimos paths
+        if os.path.isfile(from_path): #revisas que es un archivo
+            shutil.copy(from_path, dest_path) #si lo es, lo copias
+        else:
+            copypath(from_path, dest_path) #si no, lo recorres y haces el proceso, pero empezando desde los últimos paths
