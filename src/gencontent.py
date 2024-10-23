bc59f3509a3687721305f767f666cff9f2aa3f9b
arturogood17 <arturo_good17@hotmail.com>
2024-10-23 22:36:41 +0200

    Gencontent recursively

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,5 +1,6 @@
 from markdown_blocks import markdown_to_html_node
 import os
+from pathlib import Path
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
@@ -21,6 +22,16 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as to_file:
         to_file.write(template)
 
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for item in os.listdir(dir_path_content):
+        from_path= os.path.join(dir_path_content, item)
+        dest_path= os.path.join(dest_dir_path, item)
+        if os.path.isfile(from_path):
+            dest_path= Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
 
 
 def extract_title(markdown):
